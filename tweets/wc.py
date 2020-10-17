bde6670f5e3446c7b6d12233b4c25a591dd70cf5
1adityashetty1 <adityashetty@berkeley.edu>
2020-10-17 14:34:38 -0500

    Added Custom Stopwords

--- tweets/wc.py
+++ tweets/wc.py
@@ -6,11 +6,12 @@ import altair as alt
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer 
+import webbrowser
 
 def load_data(file):
     covid=pd.read_csv(file,sep=',')
     return covid
-
+url = "https://en.wikipedia.org/wiki/Lemmatisation"
 
 ##turn csv into Dictionary Object
 st.cache(suppress_st_warning=True)
@@ -22,16 +23,33 @@ tweets = tweets[tweets.counts <=  5*min_val]
 st.cache(suppress_st_warning=True)
 raw_dic = pd.Series(tweets.counts.values,index=tweets.term).to_dict()
 
+
 #load interactivity elements
 st.cache(suppress_st_warning=True)
 st.header('Word Usage in Covid-19 Tweets (April 2020)')
 st.sidebar.write("Choose Word Cloud Options")
 remove_eng = st.sidebar.checkbox("Remove English Stop Words")
 remove_esp = st.sidebar.checkbox("Remove Spanish Stop Words")
-lemma = st.sidebar.checkbox("Lemmatize")
+
 show_chart = st.button('Show Distribution')
 slider_ph = st.empty()
 value = slider_ph.slider("Choose threshold", min_value=min_val, max_value=5*min_val, step=10)
+
+#user input
+custom = st.sidebar.text_input('Add Custom Stopwords (comma separated)')
+custom = custom.split(',')
+
+lemma = st.sidebar.checkbox("Lemmatize")
+
+#create stopwords list
+st.cache(suppress_st_warning=True)
+stop_words = []
+if(custom):
+    stop_words += custom
+if(remove_eng):
+    stop_words +=  stopwords.words('english')
+if(remove_esp):
+    stop_words +=  stopwords.words('spanish')
 
 
 #create chart
@@ -41,29 +59,25 @@ basic_chart = alt.Chart(tweets[tweets['counts'] <= value]).mark_line().encode(
     y='counts'
 )
 
-#create stopwords list
-st.cache(suppress_st_warning=True)
-stop_words = []
-if(remove_eng):
-    stop_words +=  stopwords.words('english')
-if(remove_esp):
-    stop_words +=  stopwords.words('spanish')
 
 #create lemmatized dictionary
 st.cache(suppress_st_warning=True) 
 lemmatizer = WordNetLemmatizer()
 lemma_dic = {lemmatizer.lemmatize(k.strip()): v for k, v in raw_dic.items() if type(k) == str}
 
-#choose dictionary to generate wordcloud grom
+#choose dictionary to generate wordcloud
 st.cache(suppress_st_warning=True)   
 if(lemma):
-    dic = {k:v for k,v in lemma_dic.items() if v <= value}
+    dic = {k:v for k,v in lemma_dic.items() if v <= value and k not in stop_words}
+    st.sidebar.write("Words will be Lemmatized")
+    if st.sidebar.button("More Info (External Link)"):
+        webbrowser.open_new_tab(url)
 else:
-    dic = {k:v for k,v in raw_dic.items() if v <= value}
+    dic = {k:v for k,v in raw_dic.items() if v <= value and k not in stop_words}
 
 #create wordcloud
 st.cache(suppress_st_warning=True)
-wordcloud = WordCloud(stopwords = stop_words).generate_from_frequencies(frequencies=dic)
+wordcloud = WordCloud().generate_from_frequencies(frequencies=dic)
 fig = plt.figure()
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
