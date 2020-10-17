599d3339ad57e39e26458d66e78b0497d6f7742d
Michael Wang <yuanxin.wang97@gmail.com>
2020-10-16 18:24:58 -0700

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,100 @@
+from datetime import datetime
+from datetime import date
+
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+######################################
+# Load data and cache
+######################################
+# @st.cache(allow_output_mutation=True)
+def load(inp):
+    return pd.read_csv(inp)
+# https://www.kaggle.com/sudalairajkumar/novel-corona-virus-2019-dataset?select=covid_19_data.csv
+df = load("covid_19_data.csv")
+# Some manipulations for date and months
+df['Date'] = pd.to_datetime(df['Date'])
+df = df[df['Date'] < date.today()]
+df['Date'] = df['Date'].dt.strftime('%Y-%m-%d')
+df["month"] = pd.DatetimeIndex(df['Date']).month
+
+######################################
+# Introductory text
+######################################
+st.title('Covid-19 Interactive Dashboard')
+# st.sidebar.markdown('''
+# Dataset reference:
+#
+# Designers: Aditya and Yuanxin
+#
+# Functionalities:
+# ''')
+
+######################################
+# Add different fileters for users
+# Generate a filtered df
+######################################
+st.subheader("For users")
+# Select Date Range
+st.write("Please specify how many months you want to view (Default last 2 months)")
+last_month_value = st.slider("",
+                             min_value=0, max_value=6, value=2, step=1)
+max_month = df["month"].max()
+df = df[df['month'] > max_month - last_month_value]
+
+# Filter by Region
+st.write("Please select contries of interest")
+region_names = st.multiselect(label='', options=list(df.Region.unique())[::-1], default=["Mainland China", "Canada"])
+if len(region_names) > 0:
+    df = df[df['Region'].isin(region_names)]
+
+
+######################################
+# Create interactive, multi-viewed (each view communicate with each other) plots
+# histogram/bar plot, scatter plot, cascaded line plot, maps, heatmap
+######################################
+# Part I: Curves for different contries
+st.subheader('Comparision of different countries')
+st.write("Select metrics to view")
+option = st.selectbox('', ('Confirmed', 'Deaths', 'Recovered'))
+number_versus_time  =alt.Chart(df).mark_line().encode(
+    x=alt.X('Date', title='Date'),
+    y=alt.Y(option,  title=option),
+    color='Region',
+    tooltip = ['Confirmed', 'Deaths', 'Recovered'],
+).interactive()
+st.write(number_versus_time)
+
+# Part II : Find correlations
+st.subheader("Explore Correlations")
+st.write("Now let's explore the correlations between pair of variables. Select your two variables here!")
+variables = st.multiselect(label='', options=['Confirmed', 'Deaths', 'Recovered'], default=["Confirmed", "Deaths"])
+if len(variables) > 2:
+    variables = variables[:2]
+scatter = alt.Chart(df).mark_point().encode(
+    x=alt.X(variables[0], scale=alt.Scale(zero=False)),
+    y=alt.Y(variables[1], scale=alt.Scale(zero=False)),
+    color=alt.Y("Region"),
+    tooltip = ['Confirmed', 'Deaths', 'Recovered']
+).properties(
+    width=600, height=400
+).interactive()
+remainder_var = [item for item in ['Confirmed', 'Deaths', 'Recovered'] if item not in variables][0]
+picked = alt.selection_interval()
+
+bar_plots = alt.Chart(df).mark_rect().encode(
+    x="Confirmed",
+    y="Recovered",
+    color='Region',
+    tooltip = ['Confirmed', 'Deaths', 'Recovered']
+).transform_filter(picked)
+
+st.write(
+        scatter.encode(color=alt.condition(picked, "Region", alt.value("lightgrey")))
+        .add_selection(picked) & bar_plots.interactive())
+
+
+# Part III: World Map
+
+# Part IV: Word Cloud!
